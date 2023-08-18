f918d2b26cfb940e980acea0c15268f4c79ecacf
Jaewook Lee <jaewooklee042@gmail.com>
2023-08-18 17:28:25 +0200

    operator base

---
+++ src/operators/base.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <mfem.hpp>
+
+#include "mimi/utils/print.hpp"
+#include "mimi/solvers/newton.hpp"
+
+namespace mimi::operators {
+
+class OperatorBase {
+public:
+  using LinearFormPointer_ = std::shared_ptr<mfem::LinearForm>;
+  using BilinearFormPointer_ = std::shared_ptr<mfem::BilinearForm>;
+  using NonlinearFormPointer_ = std::shared_ptr<mfem::NonlinearForm>;
+
+protected:
+  mfem::FiniteElementSpace& fe_space_;
+
+  /// @brief container to hold any necessary / related linear forms
+  std::unordered_map<std::string, LinearFormPointer_> linear_forms_{};
+  /// @brief container to hold any necessary / related bilinear forms
+  std::unordered_map<std::string, BilinearFormPointer_> bilinear_forms_{};
+  /// @brief container to hold any necessary / related nonlinear forms
+  std::unordered_map<std::string, NonlinearFormPointer_> nonlinear_forms_{};
+
+  /// @brief newton solver should be configured outside of this class
+  std::shared_ptr<mimi::solvers::Newton> newton_solver_{nullptr};
+
+public:
+  /// @brief ctor
+  /// @param fe_space
+  OperatorBase(mfem::FiniteElementSpace& fe_space) : fe_space_(fe_space) {}
+
+  /// @brief adds linear form
+  /// @param key 
+  /// @param lf 
+
+  virtual void AddLinearForm(std::string const& key,
+                     const LinearFormPointer_& lf) {
+    MIMI_FUNC();
+
+    linear_forms_[key] = lf;
+  }
+
+  /// @brief adds bilinearform
+  /// @param key 
+  /// @param bf 
+
+  virtual void AddBilinearForm(std::string const& key, const BilinearFormPointer_& bf) {
+    MIMI_FUNC();
+
+    bilinear_forms_[key] = bf;
+  }
+
+  /// @brief adds nonlinear form
+  /// @param key 
+  /// @param nf 
+  virtual void AddNonlinearForm(std::string const& key,
+                        const NonlinearFormPointer_& nf) {
+    MIMI_FUNC();
+
+    nonlinear_forms_[key] = nf;
+  }
+
+  virtual void SetNewtonSolver(std::shared_ptr<mimi::solvers::LineSearchNewton> const& newton_solver) {
+    MIMI_FUNC();
+
+    newton_solver_ = newton_solver;
+  }
+
+  virtual void Setup() = 0;
+};
+
+} // namespace mimi::operators
