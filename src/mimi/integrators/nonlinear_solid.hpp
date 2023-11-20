209b779235ed6c46e15992305445d98cd38f211a
Jaewook Lee <jaewooklee042@gmail.com>
2023-11-20 15:27:41 +0100

    rough draft for precompute in nonlinear solid

--- src/mimi/integrators/nonlinear_solid.hpp
+++ src/mimi/integrators/nonlinear_solid.hpp
@@ -4,6 +4,138 @@
 
 namespace mimi::integrators {
 
-class NonliearSolid : public NonlinearBase {};
+class NonliearSolid : public NonlinearBase {
+
+  virtual const std::string& Name() const { return name_; }
+
+  /// This one needs / stores
+  /// - basis(shape) function derivative (at reference)
+  /// - reference to target jacobian weight (det)
+  /// - target to reference jacobian (inverse of previous)
+  /// - basis derivative at target
+  virtual void Prepare(const int quadrature_order = -1) {
+    MIMI_FUNC()
+
+    // get numbers to decide loop size
+    const int n_elements = precomputed_->meshes_[0]->NURBSext->GetNE();
+    const int n_threads =
+        precomputed_->meshes_
+            .size(); // n meshes should equal to nthrea config at the beginning
+
+    // get dim
+    dim_ = precomputed_->meshes_[0]->Dimension();
+
+    // allocate element vectors and matrices
+    element_matrices_ =
+        std::make_unique<mimi::utils::Data<mfem::DenseMatrix>>(n_elements);
+    element_vectors_ =
+        std::make_unique<mimi::utils::Data<mfem::Vector>>(n_elements);
+
+    // DShape - you get derivatives per dim. This is at reference
+    auto& d_shapes = precomputed_->matrices_["d_shapes"];
+    d_shapes.resize(n_elements);
+
+    // target DShape
+    auto& target_d_shapes = precomputed_->matrices_["target_d_shapes"];
+    target_d_shapes.resize(n_elements);
+
+    // element target_to_reference weight
+    auto& target_to_reference_weights =
+        precomputed_->scalars_["target_to_reference_weights"];
+    target_to_reference_weights.resize(n_elements);
+
+    // element reference_to_target jacobian (inverse of target_to_reference
+    // jacobian)
+    auto& reference_to_target_jacobians =
+        precomputed_->matrices_["reference_to_target_jacobians"];
+    reference_to_target_jacobians.resize(n_elements);
+
+    // quadrature orders
+    quadrature_orders_.resize(n_elements);
+
+    auto precompute_at_elements_and_quads = [&](const int el_begin,
+                                                const int el_end,
+                                                const int i_thread) {
+      // thread's obj
+      auto& int_rules = precomputed_->int_rules_[i_thread];
+
+      // element loop
+      for (int i{el_begin}, i < el_end, ++i) {
+        // deref i-th element of
+        // elements, eltrans
+        // std::vectors of mfem::Vector and mfem::DenseMatrix
+        const auto& i_el = precomputed_->elements_[i];
+        const auto& i_el_trans =
+            *precomputed->target_to_reference_element_trans_[i];
+        auto& i_d_shapes = d_shapes[i];
+        auto& i_target_d_shapes = target_d_shapes[i];
+        auto& i_target_to_reference_weights = target_to_reference_weights[i];
+        auto& i_reference_to_target_jacobians =
+            reference_to_target_jacobians[i];
+
+        // get quad order
+        const int q_order = (quadrature_order < 0)
+                                ? i_el->GetFE()->GetOrder() * 2 + 3
+                                : quadrature_order;
+
+        // save this quad order
+        quadrature_orders_[i] = q_order;
+
+        // get int rule
+        const mfem::IntegrationRule& ir =
+            int_rules.Get(i_el->GetGeomType(), q_order);
+        // prepare quad loop
+        const int n_quad = ir.GetNPoints();
+        const int n_dof = i_el->GetDof();
+
+        // now, allocate space
+        i_d_shapes.resize(n_quad);
+        i_target_d_shapes.resize(n_quad);
+        i_target_to_reference_weights.resize(n_quad);
+        i_reference_to_target_jacobians.resize(n_quad);
+
+        // also allocate element based vectors and matrix (assembly output)
+        element_vectors_->operator[](i).SetSize(n_dof * dim_);
+        element_matrices_->operator[](i).SetSize(n_dof * dim_, n_dof * dim_);
+
+        for (int j{}; j < n_quad; ++j) {
+          // get int point - this is just look up within ir
+          const mfem::IntegrationPoint& ip = ir.IntPoint(j);
+          i_el_trans.SetIntPoint(&ip);
+
+          // get d shapes
+          mfem::DenseMatrix& j_d_shape = i_shapes[j];
+          mfem::DenseMatrix& j_target_d_shape = i_target_d_shapes[j];
+          mfem::DenseMatrix& j_reference_to_target_jacobian =
+              i_reference_to_target_jacobians[j];
+          j_d_shape.SetSize(n_dof, dim_);
+          j_target_d_shape.SetSize(n_dof, dim_);
+          j_reference_to_target_jacobian.SetSize(dim_, dim_);
+
+          //  Calc
+          i_el->CalcDShape(ip, j_d_shape);
+          mfem::CalcInverse(i_el_trans.Jacobian(),
+                            j_reference_to_target_jacobian);
+          mfem::Mult(j_d_shape,
+                     j_reference_to_target_jacobian,
+                     j_target_d_shape);
+
+          // at last, trans weight
+          i_target_to_reference_weights[j] = i_el_trans.Weight();
+        }
+      }
+    };
+
+    mimi::utils::NThreadExe(precompute_at_elements_and_quads,
+                            n_elements,
+                            n_threads);
+  }
+
+  virtual void AssembleDomainResidual(const mfem::Vector& current_x) {
+    MIMI_FUNC()
+  }
+
+  virtual void AssembleDomainGrad(const mfem::Vector& current_x) { MIMI_FUNC() }
+};
 
 } // namespace mimi::integrators
