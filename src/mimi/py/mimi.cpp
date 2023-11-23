f437531a6037c5ebe95289977f13eba11082713e
Jaewook Lee <jaewooklee042@gmail.com>
2023-11-23 13:23:40 +0100

    compile setup - need some fixes

--- src/mimi/py/mimi.cpp
+++ src/mimi/py/mimi.cpp
@@ -7,6 +7,8 @@ void init_py_solid(py::module_&);
 void init_py_linear_elasticity(py::module_&);
 void init_py_nearest_distance(py::module_&);
 void init_py_nonlinear_form(py::module_&);
+void init_py_material(py::module_&);
+void init_py_nonlinear_solid(py::module_&);
 } // namespace mimi::py
 
 PYBIND11_MODULE(mimi, m) {
@@ -15,4 +17,6 @@ PYBIND11_MODULE(mimi, m) {
   mimi::py::init_py_linear_elasticity(m);
   mimi::py::init_py_nearest_distance(m);
   mimi::py::init_py_nonlinear_form(m);
+  mimi::py::init_py_material(m);
+  mimi::py::init_py_nonlinear_solid(m);
 }
