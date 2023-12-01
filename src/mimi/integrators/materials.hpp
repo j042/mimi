c4320c554f0062254babce8eecf7509d4dea7c4d
Jaewook Lee <jaewooklee042@gmail.com>
2023-12-01 23:03:30 +0100

    J2 rough draft

--- src/mimi/integrators/materials.hpp
+++ src/mimi/integrators/materials.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 #include <mfem.hpp>
 
 #include "mimi/utils/containers.hpp"
@@ -254,6 +256,244 @@ public:
               -mu_over_det_F + lambda_ * (det_F - 1.),
               I_,
               sigma);
+  }
+};
+
+/* define some static math helper functions often used in J2 and beyond */
+
+/// computes deviator.
+/// often used to compute stress deviator, so there's factor
+inline void Dev(const mfem::DenseMatrix& A,
+                const int dim,
+                const double factor,
+                mfem::DenseMatrix& dev_A) {
+  // get data ptr
+  const double* A_data = A.GetData();
+  double* dev_A_data = dev_A.GetData();
+
+  if (dim == 2) {
+    const double& A_0 = A_data[0];
+    const double& A_3 = A_data[3];
+    const double tr_A_over_dim = (A_0 + A_3) / dim;
+
+    dev_A_data[0] = (A_0 - tr_A_over_dim) * factor;
+    dev_A_data[1] = A_data[1] * factor;
+    dev_A_data[2] = A_data[2] * factor;
+    dev_A_data[3] = (A_3 - tr_A_over_dim) * factor;
+    return;
+  } else {
+    const double& A_0 = A_data[0];
+    const double& A_4 = A_data[3];
+    const double& A_8 = A_data[8];
+    const double tr_A_over_dim = (A_0 + A_4 + A_8) / dim;
+
+    dev_A_data[0] = (A_0 - tr_A_over_dim) * factor;
+    dev_A_data[1] = A_data[1] * factor;
+    dev_A_data[2] = A_data[2] * factor;
+    dev_A_data[3] = A_data[3] * factor;
+    dev_A_data[4] = (A_4 - tr_A_over_dim) * factor;
+    dev_A_data[5] = A_data[5] * factor;
+    dev_A_data[6] = A_data[6] * factor;
+    dev_A_data[7] = A_data[7] * factor;
+    dev_A_data[8] = (A_8 - tr_A_over_dim) * factor;
+    return;
+  }
+}
+
+/// sym(F - I) - plastic strain
+inline void ElasticStrain(const mfem::DenseMatrix& F,
+                          const mfem::DenseMatrix& plastic_strain,
+                          mfem::DenseMatrix& elastic_strain) {
+  const int dim = F.Height();
+  const double* F_data = F.GetData();
+  const double* p_data = plastic_strain.GetData();
+  double* e_data = elastic_strain.GetData();
+
+  // first, use space of e to copy F
+  std::copy_n(F_data, dim * dim, e_data);
+
+  // substract I
+  for (int i{}; i < dim; ++i) {
+    e_data[(dim + 1) * i] -= 1;
+  }
+
+  // symmetrize
+  elastic_strain.Symmetrize();
+
+  // subtract
+  for (int i{}; i < dim * dim; ++i) {
+    e_data[i] -= p_data[i];
+  }
+}
+
+/// Frobenius norm of a matrix
+inline double Norm(const mfem::DenseMatrix& A) {
+  const int dim = A.Height();
+
+  const double* A_data = A.GetData();
+  double a{};
+  for (int i{}; i < dim * dim; ++i) {
+    const double& A_i = A_data[i];
+    a += A_i * A_i;
+  }
+  return std::sqrt(a);
+}
+
+/// A[i] -= a;
+inline void Subtract(mfem::DenseMatrix& A, const double a) {
+  const int dim = A.Height();
+  double* A_data = A.GetData();
+  for (int i{}; i < dim * dim; ++i) {
+    A_data[i] += a;
+  }
+}
+
+/// A += a * I
+inline void AddDiag(mfem::DenseMatrix& A, const double a) {
+  const int dim = A.Height();
+  double* A_data = A.GetData();
+  for (int i{}; i < dim; ++i) {
+    A_data[(dim + 1) * i] += a;
+  }
+}
+
+/// @brief Computational Methods for plasticity p260, box 7.5
+/// Implementation reference from serac
+class J2 : public MaterialBase {
+public:
+  using Base_ = MaterialBase;
+  using MaterialStatePtr_ = typename Base_::MaterialStatePtr_;
+
+  // additional parameters
+  double isotropic_hardening_;
+  double kinematic_hardening_;
+  double sigma_y_; // yield
+
+  /// @brief Bulk Modulus (lambda + 2 / 3 mu)
+  double K_; // K
+  /// shear modulus (=mu)
+  double G_;
+
+  /// decided to use inline
+  bool is_2d_{true};
+
+  struct State : public MaterialState {
+    static constexpr const int k_state_matrices{2};
+    static constexpr const int k_state_scalars{2};
+    /// matrix indices
+    static constexpr const int k_beta{0};
+    static constexpr const int k_plastic_strain{1};
+    /// scalar indices
+    static constexpr const int k_accumulated_plastic_strain{0};
+  };
+
+protected:
+  /// I am thread-safe. Don't touch me after Setup
+  mfem::DenseMatrix I_;
+
+  /// some constants
+  const double sqrt_6_ = std::sqrt(6.0);
+  const double sqrt_3_2_ = std::sqrt(3.0 / 2.0);
+  const double sqrt_2_3_ = std::sqrt(2.0 / 3.0);
+
+  /// lookup index for matrix
+  /// elastic strain
+  static constexpr const int k_eps{0};
+  /// stress deviator
+  static constexpr const int k_s{1};
+  /// elastic trial relative stress
+  static constexpr const int k_eta{2};
+
+public:
+  virtual std::string Name() const { return "J2"; }
+
+  /// gives hint to integrator, which stress is implemented we need
+  virtual bool UsesCauchy() const {
+    MIMI_FUNC()
+    return true;
+  }
+
+  virtual void Setup(const int dim, const int nthread) {
+    MIMI_FUNC()
+
+    /// base setup for conversions and dim, nthread
+    Base_::Setup(dim, nthread);
+
+    // I
+    I_.Diag(1., dim);
+
+    // match variable name with the literature
+    K_ = lambda_ + (2 * mu_ / 3);
+    G_ = mu_;
+
+    /// make space for du_dX
+    aux_matrices_.resize(
+        n_threads_,
+        Vector_<mfem::DenseMatrix>(3, mfem::DenseMatrix(dim_)));
+  }
+
+  virtual MaterialStatePtr_ CreateState() const {
+    MIMI_FUNC();
+
+    std::shared_ptr<State> state = std::make_shared<State>();
+    // create 2 matrices with the size of dim x dim and zero initialize
+    state->matrices_.resize(state->k_state_matrices);
+    for (mfem::DenseMatrix& mat : state->matrices_) {
+      mat.SetSize(dim_, dim_);
+      mat = 0.;
+    }
+    // one scalar, also zero
+    state->scalars_.resize(state->k_state_scalars, 0.);
+    return state;
+  };
+
+  virtual void EvaluateCauchy(const mfem::DenseMatrix& F,
+                              const int& i_thread,
+                              MaterialStatePtr_& state,
+                              mfem::DenseMatrix& sigma) {
+    MIMI_FUNC()
+
+    // get aux
+    auto& i_aux = aux_matrices_[i_thread];
+    mfem::DenseMatrix& eps = i_aux[k_eps];
+    mfem::DenseMatrix& s = i_aux[k_s];
+    mfem::DenseMatrix& eta = i_aux[k_eta];
+
+    // get states
+    mfem::DenseMatrix& beta = state->matrices_[State::k_beta];
+    mfem::DenseMatrix& plastic_strain =
+        state->matrices_[State::k_plastic_strain];
+    double& accumulated_plastic_strain =
+        state->scalars_[State::k_accumulated_plastic_strain];
+
+    // precompute aux values
+    // eps, p, s, eta, q, phi
+    ElasticStrain(F, state->matrices_, dim_, eps);
+    const double p = K_ * eps.Trace();
+    Dev(eps, dim_, 2.0 * G_, s);
+    mfem::Add(s, beta, -1., eta);
+    const double eta_norm = Norm(eta, dim_ * dim_);
+    const double q = sqrt_3_2_ * eta_norm;
+    const double phi =
+        q - (sigma_y_ + isotropic_hardening_ * accumulated_plastic_strain);
+
+    if (phi > 0.) {
+      // 7.207
+      const double plastic_strain_inc =
+          phi / (3. * G_ * kinematic_hardening_ + isotropic_hardening_);
+
+      // normalize eta in-place as we don't need it anymore
+      eta *= 1. / eta_norm;
+
+      // return mapping
+      s.Add(-sqrt_6_ * G_ * plastic_strain_inc, eta);
+      accumulated_plastic_strain += plastic_strain_inc;
+      plastic_strain.Add(sqrt_3_2_ * plastic_strain_inc, eta);
+      beta.Add(sqrt_2_3_ * kinematic_hardening_ * plastic_strain_inc, eta);
+    }
+
+    // returning s + p * I
+    mfem::Add(s, I_, p, sigma);
   }
 };
 
