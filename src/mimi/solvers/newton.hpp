815dd3d841d603c6d9cfa11d891427de3da3cc5a
Jaewook Lee <jaewooklee042@gmail.com>
2023-12-06 21:30:08 +0100

    add scalar solve - adapted from serac

--- src/mimi/solvers/newton.hpp
+++ src/mimi/solvers/newton.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <tuple>
+
 #include <mfem.hpp>
 
 #include "mimi/integrators/materials.hpp"
+#include "mimi/utils/ad.hpp"
 #include "mimi/utils/print.hpp"
 
 namespace mimi::solvers {
@@ -170,5 +173,136 @@ public:
     }
   }
 };
+
+struct ScalarSolverOptions {
+  double xtol;           ///< absolute tolerance on Newton correction
+  double rtol;           ///< absolute tolerance on absolute value of residual
+  unsigned int max_iter; ///< maximum allowed number of iterations
+};
+
+/// implementation taken from serac
+/// thank you serac
+/// ParamTypes should be just simple double
+template<typename function, typename... ParamTypes>
+auto ScalarSolve(function&& f,
+                 double x0,
+                 double lower_bound,
+                 double upper_bound,
+                 ScalarSolverOptions options,
+                 ParamTypes... params) {
+
+  using AD = mimi::utils::ADScalar<double, 1>;
+  // param types should be a double.
+  double x, df_dx;
+  double fl = f(lower_bound, params...);
+  double fh = f(upper_bound, params...);
+
+  if (fl * fh > 0.) {
+    mimi::utils::PrintAndThrowError(
+        "ScalarSolve: root not bracketed by input bounds.");
+  }
+
+  unsigned int iterations = 0;
+  bool converged = false;
+
+  // handle corner cases where one of the brackets is the root
+  if (fl == 0) {
+    x = lower_bound;
+    converged = true;
+  } else if (fh == 0) {
+    x = upper_bound;
+    converged = true;
+  }
+
+  if (converged) {
+    df_dx = f(AD(x, 0), params...).GetDerivatives(0);
+
+  } else {
+    // orient search so that f(xl) < 0
+    double xl = lower_bound;
+    double xh = upper_bound;
+    if (fl > 0) {
+      xl = upper_bound;
+      xh = lower_bound;
+    }
+
+    // move initial guess if it is not between brackets
+    if (x0 < lower_bound || x0 > upper_bound) {
+      x0 = 0.5 * (lower_bound + upper_bound);
+    }
+
+    x = x0;
+    double delta_x_old = std::abs(upper_bound - lower_bound);
+    double delta_x = delta_x_old;
+    auto R = f(AD(x, 0), params...);
+    auto fval = R.GetValue();
+    df_dx = R.GetDerivatives(0);
+
+    while (!converged) {
+      if (iterations == options.max_iter) {
+        mimi::utils::PrintWarning(
+            "ScalarSolve: failed to converge in allotted iterations.");
+        break;
+      }
+
+      // use bisection if Newton oversteps brackets or is not decreasing
+      // sufficiently
+      if ((x - xh) * df_dx - fval > 0 || (x - xl) * df_dx - fval < 0
+          || std::abs(2. * fval) > std::abs(delta_x_old * df_dx)) {
+        delta_x_old = delta_x;
+        delta_x = 0.5 * (xh - xl);
+        x = xl + delta_x;
+        converged = (x == xl);
+      } else { // use Newton step
+        delta_x_old = delta_x;
+        delta_x = fval / df_dx;
+        auto temp = x;
+        x -= delta_x;
+        converged = (x == temp);
+      }
+
+      // function and jacobian evaluation
+      R = f(AD(x, 0), params...);
+      fval = R.GetValue();
+      df_dx = R.GetDerivatives(0);
+
+      // convergence check
+      converged = converged || (std::abs(delta_x) < options.xtol)
+                  || (std::abs(fval) < options.rtol);
+
+      // maintain bracket on root
+      if (fval < 0) {
+        xl = x;
+      } else {
+        xh = x;
+      }
+
+      ++iterations;
+    }
+  }
+
+  // Accumulate derivatives so that the user can get derivatives
+  // with respect to parameters, subject to constraing that f(x, p) = 0 for all
+  // p Conceptually, we're doing the following: [fval, df_dp] = f(get_value(x),
+  // p) df = 0 for p in params:
+  //   df += inner(df_dp, dp)
+  // dx = -df / df_dx
+  // constexpr bool contains_duals =
+  //     (is_dual_number<ParamTypes>::value || ...) ||
+  //     (is_tensor_of_dual_number<ParamTypes>::value || ...);
+  // if constexpr (contains_duals) {
+  //   auto [fval, df] = f(x, params...);
+  //   auto         dx = -df / df_dx;
+  //   SolverStatus status{.converged = converged, .iterations = iterations,
+  //   .residual = fval}; return tuple{dual{x, dx}, status};
+  // }
+  // if constexpr (!contains_duals) {
+  //   auto         fval = f(x, params...);
+  //   SolverStatus status{.converged = converged, .iterations = iterations,
+  //   .residual = fval}; return std::tuple{x, status};
+  // }
+
+  return x;
+}
 
 } // namespace mimi::solvers
