2b9d699bb4868b445426cb1a63ed69c4dcf4945d
Jaewook Lee <jaewooklee042@gmail.com>
2024-01-05 16:25:20 +0100

    add/edit examples

--- examples/jc_sr.py
+++ examples/jc_sr.py
@@ -1,29 +1,35 @@
 import splinepy as sp
 import mimi
 import gustaf as gus
+import numpy as np
+
 
 sp.settings.NTHREADS = 4
 
 #  create nl solid
-nl = mimi.PyNonlinearSolid()
+nl = mimi.PyNonlinearViscoSolid()
 nl.read_mesh("tests/data/balken.mesh")
 # refine
 nl.elevate_degrees(1)
 nl.subdivide(3)
 
 # create material
-mat = mimi.PyJ2NonlinearIsotropicHardening()
-mat.density = 10
+mat = mimi.PyJ2ViscoIsotropicHardening()
+mat.density = 1
 
 
-mat.viscosity = 10
+mat.viscosity = -10
 mat.lambda_ = 790000 - (79000 * 2 / 3)
-mat.mu = 79000
+mat.lambda_ *= 10
+mat.mu = 79000 * 10
 
-mat.hardening = mimi.PyJohnsonCookHardening()
+mat.hardening = mimi.PyJohnsonCookViscoHardening()
 mat.hardening.A = 100
 mat.hardening.B = mat.hardening.A * 2.5
 mat.hardening.n = 0.2835
+mat.hardening.C = 0.034
+mat.hardening.eps0_dot = 0.004
+
 
 nl.set_material(mat)
 
@@ -34,19 +40,21 @@ s.cps[:] = s.cps[to_s]
 
 bc = mimi.BoundaryConditions()
 bc.initial.dirichlet(2, 0).dirichlet(2, 1)
-bc.initial.body_force(1, -100)
-# bc.initial.traction(3, 1, -100)
+bc.initial.body_force(1, -10)
+#bc.initial.traction(3, 1, -1000)
 
 nl.boundary_condition = bc
 
 nl.setup(1)
-nl.configure_newton("nonlinear_solid", 1e-12, 1e-8, 40, True)
+nl.configure_newton("nonlinear_visco_solid", 1e-12, 1e-8, 40, False)
 
 rhs = nl.linear_form_view2("rhs")
 print(rhs)
 
 nl.time_step_size = 0.005
+x_ref = nl.solution_view("displacement", "x_ref").reshape(-1, nl.mesh_dim())
 x = nl.solution_view("displacement", "x").reshape(-1, nl.mesh_dim())
+v = nl.solution_view("displacement", "x_dot").reshape(-1, nl.mesh_dim())
 s.show_options["control_point_ids"] = False
 s.show_options["control_points"] = False
 # s.show_options["knots"] = False
@@ -64,13 +72,14 @@ for i in range(10000):
             interactive=False,
         )
     # remove body force
-    if i == 75:
-        rhs[:] *= -1.0
-    if i == 150:
-        rhs[:] = 0.0
+    #if i == 75:
+    #    rhs[:] *= -1.0
+    #if i == 150:
+    #    rhs[:] = 0.0
 
     nl.step_time2()
-    print(i)
-    print(x[:4])
+    print(i, np.linalg.norm(x-x_ref))
+#    print(x[:4])
+#    print(v[:4])
 
 gus.show(s, vedoplot=plt, interactive=True)
