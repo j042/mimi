9c2841cb9cbeffb2790c9bc0ccf03da97cf7b514
Guenther Obermair <guenther.obermair@tuwien.ac.at>
2024-01-12 12:02:57 +0100

    Merged classes together

--- src/mimi/solvers/ode.hpp
+++ src/mimi/solvers/ode.hpp
@@ -8,8 +8,6 @@
 
 namespace mimi::solvers {
 
-// GENERAL
-
 class OdeBase {
 public:
   virtual ~OdeBase() = default;
@@ -53,8 +51,6 @@ public:
                                     Name());
   }
 };
-
-// SOLVER 1
 
 class GeneralizedAlpha2 : public mfem::GeneralizedAlpha2Solver, public OdeBase {
 protected:
@@ -159,26 +155,38 @@ public:
   }
 };
 
-// SOLVER 2
-
 // Imported the parts of Newmark from MFEM manually
 
 /// The classical newmark method.
 /// Newmark, N. M. (1959) A method of computation for structural dynamics.
 /// Journal of Engineering Mechanics, ASCE, 85 (EM3) 67-94.
-class NewmarkSolver : public mfem::SecondOrderODESolver {
+class NewmarkSolver : public mfem::SecondOrderODESolver, public OdeBase {
 protected:
   mfem::Vector d2xdt2;
   mfem::Vector xa, va, aa;
-  double alpha_f, alpha_m, beta, gamma;
+  double beta, gamma;
+  double fac0_, fac1_, fac2_, fac3_, fac4_, fac5_;
   int nstate;
   bool first;
 
 public:
-  NewmarkSolver(double beta_ = 0.25, double gamma_ = 0.5) {
-    beta = beta_;
-    gamma = gamma_;
-  };
+  using Base_ = NewmarkSolver;
+
+  NewmarkSolver(mfem::SecondOrderTimeDependentOperator& oper,
+          double beta = 0.25,
+          double gamma = 0.5)
+      //: Base_(beta, gamma) 
+      {
+
+    Base_::Init(oper);
+
+    // compute factors
+    fac0_ = 0.5 - beta;
+    fac2_ = 1.0 - gamma;
+    fac3_ = beta;
+    fac4_ = gamma;
+    //fac5_ = alpha_m;
+  }
 
   virtual void Init(mfem::SecondOrderTimeDependentOperator& f_) {
     mfem::SecondOrderODESolver::Init(f_);
@@ -211,11 +219,6 @@ public:
 
   virtual void
   Step(mfem::Vector& x, mfem::Vector& dxdt, double& t, double& dt) {
-    double fac0 = 0.5 - beta;
-    double fac2 = 1.0 - gamma;
-    double fac3 = beta;
-    double fac4 = gamma;
-    double fac5 = alpha_m;
 
     // In the first pass compute d2xdt2 directly from operator.
     if (first) {
@@ -225,46 +228,24 @@ public:
     f->SetTime(t + dt);
 
     x.Add(dt, dxdt);
-    x.Add(fac0 * dt * dt, d2xdt2);
-    dxdt.Add(fac2 * dt, d2xdt2);
+    x.Add(fac0_ * dt * dt, d2xdt2);
+    dxdt.Add(fac2_ * dt, d2xdt2);
 
     f->SetTime(t + dt);
-    f->ImplicitSolve(fac3 * dt * dt, fac4 * dt, x, dxdt, d2xdt2);
+    f->ImplicitSolve(fac3_ * dt * dt, fac4_ * dt, x, dxdt, d2xdt2);
 
-    x.Add(fac3 * dt * dt, d2xdt2);
-    dxdt.Add(fac4 * dt, d2xdt2);
+    x.Add(fac3_ * dt * dt, d2xdt2);
+    dxdt.Add(fac4_ * dt, d2xdt2);
     t += dt;
   }
-};
-
-class Newmark : public NewmarkSolver, public OdeBase {
-protected:
-  double fac0_, fac1_, fac2_, fac3_, fac4_, fac5_;
-
-public:
-  using Base_ = NewmarkSolver;
-
-  Newmark(mfem::SecondOrderTimeDependentOperator& oper,
-          double beta = 0.25,
-          double gamma = 0.5)
-      : Base_(beta, gamma) {
-    Base_::Init(oper);
-
-    // compute factors
-    fac0_ = 0.5 - beta;
-    fac2_ = 1.0 - gamma;
-    fac3_ = beta;
-    fac4_ = gamma;
-    fac5_ = alpha_m;
-  }
-
+  
   virtual std::string Name() { return "Newmark"; }
 
   virtual void PrintInfo() {
     MIMI_FUNC()
 
     mimi::utils::PrintInfo("Info for", Name());
-    // Base_::PrintProperties();
+    Base_::PrintProperties();
   }
 
   virtual void
@@ -273,6 +254,7 @@ public:
 
     Base_::Step(x, dxdt, t, dt);
   }
+
   virtual void
   FixedPointSolve2(mfem::Vector& x, mfem::Vector& dxdt, double& t, double& dt) {
     MIMI_FUNC()
@@ -338,6 +320,110 @@ public:
 
     t += dt;
   }
+
 };
+/*
+class Newmark : public NewmarkSolver, public OdeBase {
+protected:
+  double fac0_, fac1_, fac2_, fac3_, fac4_, fac5_;
+
+public:
+  using Base_ = NewmarkSolver;
+  
+  Newmark(mfem::SecondOrderTimeDependentOperator& oper,
+          double beta = 0.25,
+          double gamma = 0.5)
+      : Base_(beta, gamma) {
+    Base_::Init(oper);
+
+    // compute factors
+    fac0_ = 0.5 - beta;
+    fac2_ = 1.0 - gamma;
+    fac3_ = beta;
+    fac4_ = gamma;
+    fac5_ = alpha_m;
+  }
+
+  virtual std::string Name() { return "Newmark"; }
+
+  virtual void PrintInfo() {
+    MIMI_FUNC()
+
+    mimi::utils::PrintInfo("Info for", Name());
+    // Base_::PrintProperties();
+  }
+
+  virtual void
+  StepTime2(mfem::Vector& x, mfem::Vector& dxdt, double& t, double& dt) {
+    MIMI_FUNC()
+
+    Base_::Step(x, dxdt, t, dt);
+  } 
+  virtual void
+  FixedPointSolve2(mfem::Vector& x, mfem::Vector& dxdt, double& t, double& dt) {
+    MIMI_FUNC()
+
+    // In the first pass, compute d2xdt2 directly from operator.
+    if (nstate == 0) {
+      f->Mult(x, dxdt, d2xdt2);
+      nstate = 1;
+    }
+
+    // Predict alpha levels
+    add(dxdt, fac0_ * dt, d2xdt2, va);
+    add(x, fac1_ * dt, va, xa);
+    add(dxdt, fac2_ * dt, d2xdt2, va);
+
+    // Solve alpha levels
+    f->SetTime(t + dt);
+    f->ImplicitSolve(fac3_ * dt * dt, fac4_ * dt, xa, va, aa);
+  }
+
+  virtual void FixedPointAdvance2(mfem::Vector& x,
+                                  mfem::Vector& dxdt,
+                                  double& t,
+                                  double& dt) {
+    MIMI_FUNC()
+
+    // xa and va are always freshly overwritten in fixedpointsolve,
+    // but would duplicate in AdvanceTime2, so assign a temp vector
+    mfem::Vector tmp_xa(x.Size());
+    mfem::Vector tmp_va(dxdt.Size());
+
+    // Correct alpha levels
+    // xa.Add(fac3_ * dt * dt, aa); // <- do this
+    add(xa, fac3_ * dt * dt, Base_::aa, tmp_xa);
+    // va.Add(fac4_ * dt, aa); // <- do this
+    add(va, fac4_ * dt, Base_::aa, tmp_va);
+
+    // extrapolate using temp vectors
+    x *= 1.0 - 1.0 / fac1_;
+    x.Add(1.0 / fac1_, tmp_xa);
+
+    dxdt *= 1.0 - 1.0 / fac1_;
+    dxdt.Add(1.0 / fac1_, tmp_va);
+  }
+
+  virtual void
+  AdvanceTime2(mfem::Vector& x, mfem::Vector& dxdt, double& t, double& dt) {
+    MIMI_FUNC()
+
+    // Correct alpha levels
+    xa.Add(fac3_ * dt * dt, aa);
+    va.Add(fac4_ * dt, aa);
+
+    // Extrapolate
+    x *= 1.0 - 1.0 / fac1_;
+    x.Add(1.0 / fac1_, xa);
+
+    dxdt *= 1.0 - 1.0 / fac1_;
+    dxdt.Add(1.0 / fac1_, va);
+
+    d2xdt2 *= 1.0 - 1.0 / fac5_;
+    d2xdt2.Add(1.0 / fac5_, aa);
+
+    t += dt;
+  }
+};*/
 
 } // namespace mimi::solvers
