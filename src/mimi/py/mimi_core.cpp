dd761284cb7db36270c1a8a28173e11dc64985c9
Jaewook Lee <jaewooklee042@gmail.com>
2024-07-05 12:26:00 +0200

    rm linear elasticity

--- src/mimi/py/mimi_core.cpp
+++ src/mimi/py/mimi_core.cpp
@@ -4,7 +4,6 @@ namespace mimi::py {
 namespace py = pybind11;
 void init_py_boundary_conditions(py::module_&);
 void init_py_solid(py::module_&);
-void init_py_linear_elasticity(py::module_&);
 void init_py_nearest_distance(py::module_&);
 void init_py_material(py::module_&);
 void init_py_nonlinear_solid(py::module_&);
@@ -16,7 +15,6 @@ void init_py_runtime_communication(py::module_&);
 PYBIND11_MODULE(mimi_core, m) {
   mimi::py::init_py_boundary_conditions(m);
   mimi::py::init_py_solid(m);
-  mimi::py::init_py_linear_elasticity(m);
   mimi::py::init_py_nearest_distance(m);
   mimi::py::init_py_material(m);
   mimi::py::init_py_nonlinear_solid(m);
