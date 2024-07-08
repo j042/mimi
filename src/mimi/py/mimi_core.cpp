b5ac8905dcdb75118f83a6347b438dd41cdcbb4e
Jaewook Lee <jaewooklee042@gmail.com>
2024-07-08 09:24:43 +0200

    rm visco solid

--- src/mimi/py/mimi_core.cpp
+++ src/mimi/py/mimi_core.cpp
@@ -7,7 +7,6 @@ void init_py_solid(py::module_&);
 void init_py_nearest_distance(py::module_&);
 void init_py_material(py::module_&);
 void init_py_nonlinear_solid(py::module_&);
-void init_py_nonlinear_visco_solid(py::module_&);
 void init_py_ad(py::module_&);
 void init_py_runtime_communication(py::module_&);
 } // namespace mimi::py
@@ -18,7 +17,6 @@ PYBIND11_MODULE(mimi_core, m) {
   mimi::py::init_py_nearest_distance(m);
   mimi::py::init_py_material(m);
   mimi::py::init_py_nonlinear_solid(m);
-  mimi::py::init_py_nonlinear_visco_solid(m);
   mimi::py::init_py_ad(m);
   mimi::py::init_py_runtime_communication(m);
 }
