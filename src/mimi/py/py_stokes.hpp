1c8bf335e200ebf6cd46d42020e65fb4fba03a10
markriegler <mark.riegler40@gmail.com>
2024-07-16 11:19:02 +0200

    Modify py_stokes (WIP)

--- src/mimi/py/py_stokes.hpp
+++ src/mimi/py/py_stokes.hpp
@@ -89,7 +89,6 @@ public:
     // we need to create precomputed data one for:
     // - bilinear forms
     // - nonlinear solid
-    // - (optional) conatct
     // here, we only create one for bilinear. others are done below
     // note, for bilinear, nothing is really saved. just used for thread safety
     mimi::utils::PrintDebug("Creating PrecomputedData");
@@ -108,24 +107,31 @@ public:
     Base_::FindBoundaryDofIds();
 
     // create element data - simple domain precompute
-    disp_fes.precomputed_->CreateElementQuadData("domain", nullptr);
+    velocity_fes.precomputed_->CreateElementQuadData("domain", nullptr);
+    pressure_fes.precomputed_->CreateElementQuadData("domain", nullptr);
     // once we have parallel bilinear form assembly we can re directly cal;
     // precompute
 
     // creating operators
-    auto nl_oper =
-        std::make_unique<mimi::operators::NonlinearSolid>(*disp_fes.fe_space_);
+    auto fluid_oper =
+        std::make_unique<mimi::operators::IncompressibleFluid>(
+          *velocity_fes.fe_space_,
+          *pressure_fes.fe_space_
+        );
 
     // let's setup the system
     // create dummy sparse matrix - mfem just sets referencee, so no copies are
     // made
     mfem::SparseMatrix tmp;
+    mfem::SparseMatrix tmp_rect;
 
-    // 1. mass
-    auto mass = std::make_shared<mfem::BilinearForm>(disp_fes.fe_space_.get());
-    nl_oper->AddBilinearForm("mass", mass);
+    // 1. velocity diffusion
+    auto vel_diffusion = std::make_shared<mfem::BilinearForm(velocity_fes.fe_space_.get());
+    fluid_oper->AddBilinearForm("vel_diffusion", vel_diffusion);
+    assert(material_->density_ > 0.0);
 
-    // create density coeff for mass integrator
+    // create viscosity and density coeff for integrator
+    assert(material_->viscosity_ > 0.0);
     assert(material_->density_ > 0.0);
 
     // temporarily disable custom parallel integrators
@@ -137,16 +143,19 @@ public:
 
     // create integrator using density
     // this is owned by bilinear form, so do that first before we forget
-    mfem::ConstantCoefficient rho(material_->density_);
-    mass->AddDomainIntegrator(new mfem::VectorMassIntegrator(rho));
+    mfem::ConstantCoefficient mu(material_->viscosity_);
+    mass->AddDomainIntegrator(new mfem::VectorDiffusion(mu));
     mass->Assemble(0);
-    mass->FormSystemMatrix(disp_fes.zero_dofs_, tmp);
+    mass->FormSystemMatrix(velocity_fes.zero_dofs_, tmp);
 
-    // 2. damping / viscosity, as mfem calls it
+    // 2. presure gradient and mass conservation
     if (material_->viscosity_ > 0.0) {
-      auto visc =
-          std::make_shared<mfem::BilinearForm>(disp_fes.fe_space_.get());
-      nl_oper->AddBilinearForm("viscosity", visc);
+      auto conservation =
+          std::make_shared<mfem::MixedBilinearForm>(
+            velocity_fes.fe_space_.get(),
+            pressure_fes.fe_space_.get()
+          );
+      fluid_oper->AddMixedBilinearForm("conservation", conservation);
 
       // create viscosity coeff
       // auto& visc_coeff = Base_::coefficients_["viscosity"];
@@ -154,42 +163,18 @@ public:
       // std::make_shared<mfem::ConstantCoefficient>(material_->viscosity_);
       // auto visc_integ = new mimi::integrators::VectorDiffusion(visc_coeff);
 
-      // create integrator using viscosity
-      // and add it to bilinear form
-      mfem::ConstantCoefficient v_coef(material_->viscosity_);
-      visc->AddDomainIntegrator(new mfem::VectorDiffusionIntegrator(v_coef));
+      // create integrator and add to mixed bilinear form
+      visc->AddDomainIntegrator(new mfem::VectorDivergenceIntegrator());
       visc->Assemble(0);
-      visc->FormSystemMatrix(disp_fes.zero_dofs_, tmp);
+      visc->FormRectangularSystemMatrix(
+        velocity_fes.zero_dofs_,
+        pressure_fes.zero_dofs_,
+        tmp_rect
+      );
     }
 
-    // 3. nonlinear stiffness
-    // first pre-computed
-    // nlform
-    auto nonlinear_stiffness =
-        std::make_shared<mimi::forms::Nonlinear>(disp_fes.fe_space_.get());
-    // add it to operator
-    nl_oper->AddNonlinearForm("nonlinear_stiffness", nonlinear_stiffness);
-    // create integrator
-    auto nonlinear_solid_integ =
-        std::make_shared<mimi::integrators::NonlinearSolid>(
-            material_->Name() + "-NonlinearSolid",
-            material_,
-            disp_fes.precomputed_);
-    nonlinear_solid_integ->runtime_communication_ = RuntimeCommunication();
-    // we precompute for nonlinear solid
-    const int default_solid_q =
-        RuntimeCommunication()->GetInteger("nonlinear_solid_quadrature_order",
-                                           -1);
-    disp_fes.precomputed_->PrecomputeElementQuadData("domain",
-                                                     default_solid_q,
-                                                     true);
-    nonlinear_solid_integ->Prepare();
-    // add integrator to nl form
-    nonlinear_stiffness->AddDomainIntegrator(nonlinear_solid_integ);
-    nonlinear_stiffness->SetEssentialTrueDofs(disp_fes.zero_dofs_);
-
     // 4. linear form
-    auto rhs = std::make_shared<mfem::LinearForm>(disp_fes.fe_space_.get());
+    auto rhs = std::make_shared<mfem::LinearForm>(velocity_fes.fe_space_.get());
     bool rhs_set{false};
     // assemble body force
     if (const auto& body_force =
@@ -250,51 +235,8 @@ public:
     if (rhs_set) {
       rhs->Assemble();
       // remove dirichlet nodes
-      rhs->SetSubVector(disp_fes.zero_dofs_, 0.0);
-      nl_oper->AddLinearForm("rhs", rhs);
-    }
-
-    // check contact
-    if (const auto& contact =
-            Base_::boundary_conditions_->CurrentConfiguration().contact_;
-        contact.size() != 0) {
-
-      auto nl_form =
-          std::make_shared<mimi::forms::Nonlinear>(disp_fes.fe_space_.get());
-      nl_oper->AddNonlinearForm("contact", nl_form);
-      for (const auto& [bid, nd_coeff] : contact) {
-        // initialzie integrator with nearest distance coeff (splinepy splines)
-        auto contact_integ = std::make_shared<mimi::integrators::MortarContact>(
-            nd_coeff,
-            "contact" + std::to_string(bid),
-            disp_fes.precomputed_);
-
-        contact_integ->runtime_communication_ = RuntimeCommunication();
-
-        // let's get marked boundaries to create EQData
-        contact_integ->SetBoundaryMarker(&Base_::boundary_markers_[bid]);
-        mimi::utils::Vector<int>& marked_bdr =
-            contact_integ->MarkedBoundaryElements();
-
-        // precompute
-        disp_fes.precomputed_->CreateBoundaryElementQuadData(
-            contact_integ->Name(),
-            marked_bdr.data(),
-            static_cast<int>(marked_bdr.size()));
-        const int default_contact_q =
-            RuntimeCommunication()->GetInteger("contact_quadrature_order", -1);
-        disp_fes.precomputed_->PrecomputeElementQuadData(contact_integ->Name(),
-                                                         default_contact_q,
-                                                         false /* dNdX*/);
-
-        // set the same boundary marker. It will be internally used for nthread
-        // assemble of marked boundaries
-        // precompute basis and recurring options.
-        contact_integ->Prepare();
-
-        nl_form->AddBdrFaceIntegrator(contact_integ,
-                                      &Base_::boundary_markers_[bid]);
-      }
+      rhs->SetSubVector(velocity_fes.zero_dofs_, 0.0);
+      fluid_oper->AddLinearForm("rhs", rhs);
     }
 
     // setup solvers
@@ -307,22 +249,22 @@ public:
       lin_solver->SetMaxIter(300);
       lin_solver->SetPrintLevel(-1);
       lin_solver->SetPreconditioner(*prec);
-      Base_::linear_solvers_["nonlinear_solid"] = lin_solver;
-      Base_::linear_solvers_["nonlinear_solid_preconditioner"] = prec;
+      Base_::linear_solvers_["incompressible_fluid"] = lin_solver;
+      Base_::linear_solvers_["incompressible_fluid_preconditioner"] = prec;
     } else {
       auto lin_solver = std::make_shared<mfem::UMFPackSolver>();
-      Base_::linear_solvers_["nonlinear_solid"] = lin_solver;
+      Base_::linear_solvers_["incompressible_fluid"] = lin_solver;
     }
 
     // setup a newton solver
     auto newton = std::make_shared<mimi::solvers::LineSearchNewton>();
     // give pointer of nl oper to control line search assembly
-    newton->nl_oper_ = nl_oper.get();
-    Base_::newton_solvers_["nonlinear_solid"] = newton;
+    newton->fluid_oper_ = fluid_oper.get();
+    Base_::newton_solvers_["incompressible_fluid"] = newton;
     // basic config. you can change this using ConfigureNewton()
     newton->iterative_mode = false;
-    newton->SetOperator(*nl_oper);
-    newton->SetSolver(*Base_::linear_solvers_["nonlinear_solid"]);
+    newton->SetOperator(*fluid_oper);
+    newton->SetSolver(*Base_::linear_solvers_["incompressible_fluid"]);
     newton->SetPrintLevel(mfem::IterativeSolver::PrintLevel()
                               .Warnings()
                               .Errors()
@@ -333,27 +275,17 @@ public:
     newton->SetMaxIter(MeshDim() * 10);
 
     // finally, register newton solver to the opeartor
-    nl_oper->SetNewtonSolver(newton);
+    fluid_oper->SetNewtonSolver(newton);
 
-    // ode
-    auto odesolver =
-        std::make_unique<mimi::solvers::GeneralizedAlpha2>(*nl_oper);
-    odesolver->PrintInfo();
-    if (!Base_::boundary_conditions_->InitialConfiguration()
-             .constant_velocity_.empty()) {
-      auto dynamic_dirichlet = std::make_shared<
-          mimi::utils::TimeDependentDirichletBoundaryCondition>();
-      dynamic_dirichlet->boundary_dof_ids_ = &disp_fes.boundary_dof_ids_;
-      dynamic_dirichlet->dynamic_bc_ = Base_::boundary_conditions_.get();
-      odesolver->dynamic_dirichlet_ = dynamic_dirichlet;
-    }
+    // TODO: timestepping
     Base_::boundary_conditions_->Print();
 
     // finalize operator
-    nl_oper->Setup();
+    fluid_oper->Setup();
 
+    // TODO: timestepping
     // set dynamic system -> transfer ownership of those to base
-    Base_::SetDynamicSystem2(nl_oper.release(), odesolver.release());
+    // Base_::SetDynamicSystem(fluid_oper.release(), odesolver.release());
   }
 };
 
