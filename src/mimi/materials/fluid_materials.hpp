af4e240ad0d61b30b4dd74ff7f6e981f45e0e515
markriegler <mark.riegler40@gmail.com>
2024-07-18 10:51:46 +0200

    Add CarreauWLF material model

--- src/mimi/materials/fluid_materials.hpp
+++ src/mimi/materials/fluid_materials.hpp
@@ -4,8 +4,21 @@ namespace mimi::materials {
 
 class FluidMaterialBase {
 public:
-  double density_;
   double viscosity_; // dynamic
+
+  // Non-Newtonian, shear thinning material (e.g. polymer melt)
+  struct ShearThinningBase {
+    double cp_; // specific heat capacity
+    double density_;
+  };
+
+  // Carreau model with Williams-Landel-Ferry (WLF) temperature correction
+  struct CarreauWLF : public ShearThinningBase {
+    double a_t_;      // WLF temperature correction factor
+    double CarreauA_;
+    double CarreauB_;
+    double CarreauC_;
+  };
 
   FluidMaterialBase() = default;
 };
