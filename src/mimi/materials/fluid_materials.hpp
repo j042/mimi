397f8346c7b229863a662b66b44bd785fe7376c1
Jaewook Lee <jaewooklee042@gmail.com>
2024-07-19 17:03:15 +0200

    more base for fluid materials

--- src/mimi/materials/fluid_materials.hpp
+++ src/mimi/materials/fluid_materials.hpp
@@ -1,25 +1,65 @@
 #pragma once
 
+#include "mimi/utils/print.hpp"
+
 namespace mimi::materials {
 
-class FluidMaterialBase {
+// Non-Newtonian, shear thinning material (e.g. polymer melt)
+class ShearThinningBehavior {
 public:
+  double cp_; // specific heat capacity
+  double density_;
+};
+
+// Carreau model with Williams-Landel-Ferry (WLF) temperature correction
+class CarreauWLF : public ShearThinningBehavior {
+public:
+  double a_t_; // WLF temperature correction factor
+  double CarreauA_;
+  double CarreauB_;
+  double CarreauC_;
+};
+
+class FluidMaterialBase {
+protected:
+  std::shared_ptr<ShearThinningBehavior> shear_thinning_behavior_;
+
+public:
+  double density_;
   double viscosity_; // dynamic
 
-  // Non-Newtonian, shear thinning material (e.g. polymer melt)
-  struct ShearThinningBase {
-    double cp_; // specific heat capacity
-    double density_;
-  };
-
-  // Carreau model with Williams-Landel-Ferry (WLF) temperature correction
-  struct CarreauWLF : public ShearThinningBase {
-    double a_t_;      // WLF temperature correction factor
-    double CarreauA_;
-    double CarreauB_;
-    double CarreauC_;
-  };
-
   FluidMaterialBase() = default;
+
+  /// @brief base line viscosity
+  /// @return
+  virtual double Viscosity() const {
+    MIMI_FUNC()
+
+    return viscosity_;
+  }
+
+  /// @brief temperature dependent viscosity
+  /// @param temp
+  /// @return
+  virtual double Viscosity(const double temp) const {
+    mimi::utils::PrintAndThrowError(
+        "FluidMaterialBase - Temperature dependent viscosity not implemented");
+  }
+
+  virtual double Density() const {
+    MIMI_FUNC()
+    return density_;
+  }
+
+  virtual double Density() const {
+    mimi::utils::PrintAndThrowError(
+        "FluidMaterialBase - Temperature dependent density not implemented");
+  }
 };
+
+class ShearThinning : public FluidMaterialBase {
+public:
+  double Viscosity(const double temp) const { return {}; }
+}
+
 } // namespace mimi::materials
