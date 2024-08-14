c4aa755bba224db52bcc4f9a0f0520cf2b98b926
markriegler <mark.riegler40@gmail.com>
2024-08-14 14:33:13 +0200

    Add Stokes example with Dirichlet BCs assembled (WIP)

--- examples/stokes_dirichlet.py
+++ examples/stokes_dirichlet.py
@@ -1,6 +1,38 @@
+"""examples/stokes_dirichlet.py
+
+Poiseuille flow
+"""
+
 import splinepy as sp
 import mimi
-import gustaf as gus
+
+
+def apply_dirichlet_dofs(nl):
+    def parabolic_function(points):
+        x = points[:, 0]
+        y = points[:, 1]
+        return y * (1 - y)
+
+    s, _, _ = mimi.to_splinepy(nl.vel_nurbs())
+
+    # Currently not available in main splinepy branch
+    fi = sp.helpme.integrate.FieldIntegrator(s)
+
+    # Get values for boundary dofs
+    velx_rhs, _ = fi.apply_dirichlet_boundary_conditions(
+        function=parabolic_function, west=True, return_values=True
+    )
+
+    # Apply parabolic inflow function to left inlet
+    boundary_dofs_m = nl.boundary_dof_ids("velocity", 2, 0)
+    # pretty hacky: reordering of boundary dof ids, probably TODO
+    import numpy as np
+
+    new_ids = np.hstack(([0], np.arange(2, len(velx_rhs)), [1]))
+    boundary_dofs_m = boundary_dofs_m[new_ids]
+
+    nl.solution_view("velocity", "v")[boundary_dofs_m] = velx_rhs
+
 
 sp.settings.NTHREADS = 4
 
@@ -24,44 +56,47 @@ rc = mimi.RuntimeCommunication()
 rc.set_int("use_iterative_solver", 0)
 nl.runtime_communication = rc
 
+# Apply Dirichlet BCs everywhere but on the outlet
 bc = mimi.BoundaryConditions()
-bc.initial.dirichlet(0, 0).dirichlet(0, 1).dirichlet(1, 0).dirichlet(
-    1, 1
-).dirichlet(2, 0)
-# bc.initial.traction(2, 0, 1)
+dirichlet_bids = [0, 1, 2]
+for bid in dirichlet_bids:
+    bc.initial.dirichlet(bid, 0).dirichlet(bid, 1)
 
 nl.boundary_condition = bc
 
 nl.setup(4)
 nl.configure_newton("stokes", 1e-12, 1e-8, 10, True)
 
+apply_dirichlet_dofs(nl)
+
+nl.static_solve()
+
+# Plotting
+common_show_options = {
+    "control_points": False,
+    "cmap": "jet",
+    "scalarbar": True,
+    "lighting": "off",
+    "control_points": False,
+    "knots": False,
+}
+
 v = nl.solution_view("velocity", "v").reshape(-1, 2)
 p = nl.solution_view("pressure", "p")
 
 ps, pto_m, pto_s = mimi.to_splinepy(nl)
 s, to_m, to_s = mimi.to_splinepy(nl.vel_nurbs())
-
-# parabolic profile assignemnt to control points
-# better way is to actually assemble
-m_coord = s.cps[to_m].ravel()
-lx_dofs = nl.boundary_dof_ids("velocity", 2, 0)
-ly_dofs = nl.boundary_dof_ids("velocity", 2, 1)
-
-nl.solution_view("velocity", "v")[lx_dofs] = m_coord[ly_dofs] * (
-    1 - m_coord[ly_dofs]
-)
-
-nl.static_solve()
-
-
 s2 = s.copy()
 vs = s.copy()
-vs.cps = v[to_s]
+vs.cps = v[to_s, :]
+
 s.spline_data["v"] = vs
-s.show_options(arrow_data="v", control_points=False)
+s.show_options(
+    arrow_data="v", arrow_data_scale=1, control_points=False, knots=False
+)
 
 ps.cps = p.reshape(-1, 1)[pto_s]
 s2.spline_data["p"] = ps
-s2.show_options(data="p", control_points=False)
+s2.show_options(data="p", **common_show_options)
 
-sp.show(s, s2)
+sp.show([s, "Velocity"], [s2, "Pressure"])
